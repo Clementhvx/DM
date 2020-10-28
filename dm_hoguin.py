bb7273dc7775a213b76c8322a092efa6faba6f19
Clementhvx <73315591+Clementhvx@users.noreply.github.com>
2020-10-28 14:13:54 +0100

    Add files via upload

---
+++ dm_hoguin.py
@@ -0,0 +1,69 @@
+def insererFonction():
+    retrun input("A l'aide de ce programme vous pourrez calculer l'intégrale"
+    return input("Veuillez insérer la fonction\n")
+
+def f(x):
+    return eval(fonction)
+
+def rectangles_gauches(a, b, n):
+    resultat = (b - a) / n
+    somme = 0
+
+    for i in range(n):
+        somme += f(a)
+        a += 1 / n
+    resultat = resultat * somme
+    return resultat
+
+def rectangles_droits(a,b,n):
+    resultat = (b - a) / n
+    somme = 0
+    a += 1/n
+    for i in range(1,n+1):
+        somme += f(a)
+        a += 1 / n
+    resultat = resultat * somme
+    return resultat
+
+def rectangles_medians(a,b,n):
+    resultat = (b - a) / n
+    somme = 0
+
+    for i in range(0,n):
+        somme += f(a + a+(1/n)) / 2
+        a += 1 / n
+    resultat = resultat * somme
+    return resultat
+
+def trapezes(a,b,n):
+    resultat = ((b - a) / (2*n))
+    somme = 0
+
+    for i in range(1,n):
+        somme += f(a)
+        a += 1 / n
+    resultat = resultat * (f(a) + f(b) + 2 *somme)
+    return resultat
+
+def simpson(a,b,n):
+    resultat = ((b - a) / (6*n))
+    somme1 = 0
+    somme2 = 0
+
+    for i in range(0,n):
+        if i == 0:
+            somme2 += f(a + ((2*i + 1)*(b-a))/(2*n))
+        else:
+            somme1 += f(a + (i * (b - a)) / n)
+            somme2 += f(a + ((2*i + 1)*(b-a))/(2*n))
+    resultat = resultat * (f(a) + f(b) + 2*somme1 + 4*somme2)
+    return resultat
+
+
+fonction = entreeFonction()
+print("Méthode Rectangles gauches : " + str(rectangles_gauches(0, 1, 10)))
+print("Méthode Rectangles droits : " + str(rectangles_droits(0, 1, 10)))
+print("Méthode Rectangles médians : " + str(rectangles_medians(0, 1, 10)))
+print("Méthode des Trapèzes : " + str(trapezes(0, 1, 10)))
+print("Méthode de Simpsons : " + str(simpson(0,1,10)))
+
